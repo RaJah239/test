79b794fdd43ceea4a301299a78ef20fdea356606
Nayru62 <Nayru62crystal@gmail.com>
2023-09-27 06:13:32 -0400

    Gen3+ EVs replace Stat Exp. However max out all stats at 252. Basically, getting the same results as stat exp but also saving space.
    https://github.com/pret/pokecrystal/wiki/Replace-stat-experience-with-EVs

--- data/pokemon/base_stats/mr__mime.asm
+++ data/pokemon/base_stats/mr__mime.asm
@@ -1,16 +1,15 @@
 	db MR__MIME ; 122
 
 	db  40,  45,  65,  90, 100, 120
+	evs  0,   0,   0,   0,   0,   2
 	;   hp  atk  def  spd  sat  sdf
 
-	db PSYCHIC_TYPE, PSYCHIC_TYPE ; type
+	db PSYCHIC_TYPE,NORMAL ; type
 	db 45 ; catch rate
 	db 136 ; base exp
 	db NO_ITEM, MYSTERYBERRY ; items
 	db GENDER_F50 ; gender ratio
-	db 100 ; unknown 1
 	db 25 ; step cycles to hatch
-	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/mr__mime/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
