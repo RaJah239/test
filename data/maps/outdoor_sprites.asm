1d7f032e75284f8d1b1cc8cdfc3bb3e35fbc97ff
RaJah239 <ravijagroo239@gmail.com>
2024-06-16 12:59:14 -0400

    Eusine's sprite added throughout. Free space: 399138/2097152 (19.03%)

--- data/maps/outdoor_sprites.asm
+++ data/maps/outdoor_sprites.asm
@@ -200,8 +200,8 @@ CianwoodGroupSprites:
 	db SPRITE_SWIMMER_GUY
 	db SPRITE_SAILOR
 	db SPRITE_POKEFAN_F
-	db SPRITE_SUPER_NERD
-	; 8 of max 9 walking sprites
+	db SPRITE_EUSINE
+	; 9 of max 9 walking sprites
 	db SPRITE_MILTANK_OW
 	db SPRITE_FRUIT_TREE
 	db SPRITE_ROCK
