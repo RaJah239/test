84901babf5435c57db7a14675f0df4e0cebc1c1a
Nayru62 <Nayru62crystal@gmail.com>
2024-07-29 18:44:12 -0400

    Basic Gen 3 GFX for DEX only
    there is a bug introduced from one of the prereqs, the dex entry pointer is being corrupted
    
    cry fix

--- main.asm
+++ main.asm
@@ -682,6 +682,8 @@ SECTION "Crystal Events", ROMX
 INCLUDE "engine/events/battle_tower/load_trainer.asm"
 INCLUDE "engine/events/odd_egg.asm"
 
+SECTION "Custom GFX", ROMX
+INCLUDE "gfx/rangi_gfx.asm"
 
 SECTION "Stadium 2 Checksums", ROMX[$7DE0], BANK[$7F]
 
