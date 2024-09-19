bd086dd135bb92f793470f3c2b93bdac210bcc6b
RaJah239 <ravijagroo239@gmail.com>
2024-09-19 13:52:14 -0400

    Chansey added to Silver Cave Room2.

--- maps/SilverCaveRoom2.asm
+++ maps/SilverCaveRoom2.asm
@@ -2,6 +2,7 @@
 	const SILVERCAVEROOM2_POKE_BALL1
 	const SILVERCAVEROOM2_POKE_BALL2
 	const SILVERCAVEROOM2_POKE_BALL3
+	const SILVERCAVEROOM2_CHANSEY
 
 SilverCaveRoom2_MapScripts:
 	def_scene_scripts
@@ -19,6 +20,22 @@ SilverCaveRoom2PPUp:
 
 SilverCaveRoom2HiddenMaxPotion:
 	hiddenitem MAX_POTION, EVENT_SILVER_CAVE_ROOM_2_HIDDEN_MAX_POTION
+
+SilverCaveRoom2ChanseyScript:
+	faceplayer
+	opentext
+	writetext SilverCaveRoom2ChanseyHealText
+	cry CHANSEY
+	pause 10
+	closetext
+	special StubbedTrainerRankings_Healings
+	playsound SFX_FULL_HEAL
+	special HealParty
+	end
+	
+SilverCaveRoom2ChanseyHealText:
+	text "Seyyyy!"
+	done
 
 SilverCaveRoom2_MapEvents:
 	db 0, 0 ; filler
@@ -38,3 +55,4 @@ SilverCaveRoom2_MapEvents:
 	object_event 24, 10, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveRoom2Calcium, EVENT_SILVER_CAVE_ROOM_2_CALCIUM
 	object_event 22, 24, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveRoom2UltraBall, EVENT_SILVER_CAVE_ROOM_2_ULTRA_BALL
 	object_event  4, 20, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveRoom2PPUp, EVENT_SILVER_CAVE_ROOM_2_PP_UP
+	object_event 24, 30, SPRITE_CHANSEY_OW, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, SilverCaveRoom2ChanseyScript, -1
