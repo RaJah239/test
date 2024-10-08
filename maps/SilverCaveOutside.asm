fee0205872896dd2a07967b991aaec66ebde6bce
RaJah239 <ravijagroo239@gmail.com>
2024-10-08 15:44:03 -0400

    GS Ball replaced Full Restore outside Mt. Silver. Free space: 406793/2097152 (19.40%)

--- maps/SilverCaveOutside.asm
+++ maps/SilverCaveOutside.asm
@@ -27,8 +27,8 @@ MtSilverPokecenterSign:
 MtSilverSign:
 	jumptext MtSilverSignText
 
-SilverCaveOutsideHiddenFullRestore:
-	itemball FULL_RESTORE ;, EVENT_SILVER_CAVE_OUTSIDE_HIDDEN_FULL_RESTORE
+SilverCaveOutsideGSBall:
+	itemball GS_BALL
 
 MtSilverSignText:
 	text "MT.SILVER"
@@ -292,9 +292,8 @@ MaximaMatchAfterText:
 	def_bg_events
 	bg_event 24, 19, BGEVENT_READ, MtSilverPokecenterSign
 	bg_event 17, 13, BGEVENT_READ, MtSilverSign
-	;bg_event  9, 25, BGEVENT_ITEM, SilverCaveOutsideHiddenFullRestore
 
 	def_object_events
 	object_event -3, 19, SPRITE_RIVAL, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_RIVAL_MT_SILVER
 	object_event 27, 20, SPRITE_MAXIMA, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, MaximaScript, EVENT_MAXIMA_APPEAR_MT_SILVER_OUTSIDE
-	object_event  9, 25, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, SilverCaveOutsideHiddenFullRestore, EVENT_SILVER_CAVE_OUTSIDE_HIDDEN_FULL_RESTORE
+	object_event  9, 25, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_NPC_PINK, OBJECTTYPE_ITEMBALL, 0, SilverCaveOutsideGSBall, EVENT_CAN_GIVE_GS_BALL_TO_KURT
