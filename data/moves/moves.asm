d24bd9e4507b00968caaf01fce29d434fcefc5f3
RaJah239 <ravijagroo239@gmail.com>
2024-10-28 09:30:51 -0400

    Cross Chop updated.

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -251,7 +251,7 @@ Moves:
 	move SYNTHESIS,    EFFECT_SYNTHESIS,           0, GRASS,         STATUS,   100,  5,   0
 	move MOONLIGHT,    EFFECT_MOONLIGHT,           0, NORMAL,        STATUS,   100,  5,   0
 	move HIDDEN_POWER, EFFECT_HIDDEN_POWER,       70, NORMAL,        SPECIAL,  100, 15,   0
-	move CROSS_CHOP,   EFFECT_NORMAL_HIT,        100, FIGHTING,      PHYSICAL,  80,  5,   0
+	move CROSS_CHOP,   EFFECT_NORMAL_HIT,         70, FIGHTING,      PHYSICAL, 100, 24,   0
 	move TWISTER,      EFFECT_TWISTER,            40, DRAGON,        SPECIAL,  100, 20,  20
 	move RAIN_DANCE,   EFFECT_RAIN_DANCE,          0, WATER,         STATUS,    90,  5,   0
 	move SUNNY_DAY,    EFFECT_SUNNY_DAY,           0, FIRE,          STATUS,    90,  5,   0
