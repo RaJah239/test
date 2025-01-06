d4fb7c675c6bb992e952fcafc4a52bc8985d6e58
RaJah239 <ravijagroo239@gmail.com>
2025-01-06 05:53:35 -0400

    Added missing code to `swarm_water.asm` from the "Improving the Swarm System" tutorial; not sure why I commented it out before.. Credit: https://github.com/pret/pokecrystal/wiki/Improving-the-Swarm-System Free space: 549498/2097152 (26.20%)

--- data/wild/swarm_water.asm
+++ data/wild/swarm_water.asm
@@ -2,12 +2,12 @@
 
 SwarmWaterWildMons:
 
-       ; Example qwilfish
-;       map_id ROUTE_32
-;       db 6 percent ; encounter rate
-;	db 15, QWILFISH
-;	db 15, QWILFISH
-;	db 20, QWILFISH
-;	db 20, QWILFISH
-;
-;	db -1 ; end
+
+       map_id ROUTE_32
+       db 6 percent ; encounter rate
+	db 15, QWILFISH
+	db 15, QWILFISH
+	db 20, QWILFISH
+	db 20, QWILFISH
+
+	db -1 ; end
